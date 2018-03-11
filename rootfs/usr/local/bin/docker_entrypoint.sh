211ae51f500cac85f2b21da1f2d15436ecb9ef21
stlouisn <stlouisn@hotmail.com>
2018-03-11 19:17:45 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -1,72 +1,48 @@
 #!/bin/bash
 
-# Make sure volumes are mounted correctly
-if [[ ! -d /etc/openvpn ]]; then
-    echo -e "\nError: volume '/etc/openvpn/' not mounted.\n" >&2
+# Locate vpn configuration file
+for file in /etc/openvpn/*.ovpn /etc/openvpn/*.conf; do
+    if [[ -f $file ]]; then
+        CLIENT_CONFIG=$file
+        break
+    fi
+done
+if [[ -z $CLIENT_CONFIG ]]; then
+    echo -e "\nError: vpn configuration file in '/etc/openvpn' not found.\n" >&2
     exit 1
 fi
 
-# Check for configuration file: openvpn.conf
-if [[ ! -e /etc/openvpn/openvpn.conf ]]; then
-    echo -e "\nError: configuration file '/etc/openvpn/openvpn.conf' is missing.\n" >&2
-    exit 1
-fi
-
-# Check for configuration file: ca.crt
-if [[ ! -e /etc/openvpn/ca.crt ]]; then
-    echo -e "\nError: configuration file '/etc/openvpn/ca.crt' is missing.\n" >&2
-    exit 1
-fi
-
-# Check for configuration file: ta.key
-if [[ ! -e /etc/openvpn/ta.key ]]; then
-    echo -e "\nError: configuration file '/etc/openvpn/ta.key' is missing.\n" >&2
-    exit 1
-fi
-
-# Check for configuration file: user.crt
-if [[ ! -e /etc/openvpn/user.crt ]]; then
-    echo -e "\nError: configuration file '/etc/openvpn/user.crt' is missing.\n" >&2
-    exit 1
-fi
-
-# Check for configuration file: user.key
-if [[ ! -e /etc/openvpn/user.key ]]; then
-    echo -e "\nError: configuration file '/etc/openvpn/user.key' is missing.\n" >&2
-    exit 1
-fi
-
-# Check for environment variable: VPN_GATEWAY
+# Verify that environment variable VPN_GATEWAY is set
 if [[ -z "$VPN_GATEWAY" ]]; then
-    echo -e "\nError: Environment 'VPN_GATEWAY' needs to be set.\n" >&2
+    echo -e "\nError: Environment variable 'VPN_GATEWAY' needs to be set.\n" >&2
     exit 1
 fi
 
-# Check for environment variable: VPN_PROTOCOL
+# Verify that environment variable VPN_PROTOCOL is set
 if [[ -z "$VPN_PROTOCOL" ]]; then
-    echo -e "\nError: Environment 'VPN_PROTOCOL' needs to be set!.\n" >&2
+    echo -e "\nError: Environment variable 'VPN_PROTOCOL' needs to be set!.\n" >&2
     exit 1
 fi
 
-# Check for environment variable: VPN_PORT
+# Verify that environment variable VPN_PORT is set
 if [[ -z "$VPN_PORT" ]]; then
-    echo -e "\nError: Environment 'VPN_PORT' needs to be set!.\n" >&2
+    echo -e "\nError: Environment variable 'VPN_PORT' needs to be set!.\n" >&2
     exit 1
 fi
 
-# Check for environment variable: LAN_GATEWAY
+# Verify that environment variable LAN_GATEWAY is set
 if [[ -z "$LAN_GATEWAY" ]]; then
-    echo -e "\nError: Environment 'LAN_GATEWAY' needs to be set!.\n" >&2
+    echo -e "\nError: Environment variable 'LAN_GATEWAY' needs to be set!.\n" >&2
     exit 1
 fi
 
 # Fix user and group ownerships and secure private keys
-if [[ `mount | grep '/etc/openvpn' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nWarning: volume '/etc/openvpn/' is readonly." >&2
-else
-    chown -R root:root /etc/openvpn
-    find /etc/openvpn/. -type f -name *.key -exec chmod 0600 {} \;
-fi
+#if [[ `mount | grep '/etc/openvpn' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
+#    echo -e "\nWarning: volume '/etc/openvpn/' is readonly." >&2
+#else
+#    chown -R root:root /etc/openvpn
+#    find /etc/openvpn/. -type f -name *.key -exec chmod 0600 {} \;
+#fi
 
 # Flush all rules
 iptables -F
@@ -99,4 +75,4 @@ ip route add 192.168.0.0/16 via $LAN_GATEWAY dev eth0
 exec /usr/sbin/openvpn \
     --auth-nocache \
     --cd /etc/openvpn \
-    --config /etc/openvpn/openvpn.conf
+    --config $CLIENT_CONFIG
