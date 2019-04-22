033a8c04d182410e34cbdf89d4f26a44b7054c5f
stlouisn <stlouisn@hotmail.com>
2019-04-22 11:36:26 -0300

    Update docker.sh

--- travis/docker.sh
+++ travis/docker.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
+# Create /etc/docker directory
 mkdir -p /etc/docker
 
+# Enable overlay2 and experimental mode
 cat <<'EOF' > /etc/docker/daemon.json
 {
     "storage-driver": "overlay2",
@@ -11,13 +13,18 @@ cat <<'EOF' > /etc/docker/daemon.json
 }
 EOF
 
+# Add docker repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
+# Update apt-cache
 apt-get update -q
 
+# Update docker
 apt-get install -y docker-ce
 
+# Restart docker
 service docker restart
- 
+
+# Display docker version
 docker info
