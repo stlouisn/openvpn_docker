c271e3df73adb3439cbbb44f851991a9b574ab0b
stlouisn <stlouisn@hotmail.com>
2019-04-22 11:36:17 -0300

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -5,31 +5,33 @@ set -euo pipefail
 # Enable docker experimental client
 export DOCKER_CLI_EXPERIMENTAL="enabled"
 
+# Architectures to build
+ARCHITECTURES="arm arm64 amd64"
+
+for ARCH in $ARCHITECTURES; do
+
+	# Images to add to docker manifest list
+	DOCKER_IMAGES+="${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-${ARCH} "
+
+done
+
+# Create docker manifest list
+docker manifest create ${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} ${DOCKER_IMAGES}
+
+for ARCH in $ARCHITECTURES; do
+
+	# Annotate docker manifest list
+	docker manifest annotate --arch ${ARCH} --os linux \
+	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \
+	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-${ARCH}
+
+done
+
 # Login into docker
 echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
 
-# Create manifest list
-docker manifest create ${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-${OS_NAME}-arm \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-${OS_NAME}-arm64 \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-${OS_NAME}-amd64
-
-# Annotate manifest list
-docker manifest annotate \
-	--arch arm --os linux \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-${OS_NAME}-arm
-docker manifest annotate \
-	--arch arm64 --os linux \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-${OS_NAME}-arm64
-docker manifest annotate \
-	--arch amd64 --os linux \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-${OS_NAME}-amd64
-
-# Push manifest list
+# Push docker manifest list
 docker manifest push ${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}
 
-# Display manifest list
+# Display docker manifest list
 docker manifest inspect ${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}
