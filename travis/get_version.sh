cbeb40fd1496c3f19777eee6bacc28ac77890ad1
stlouisn <stlouisn@hotmail.com>
2020-01-30 08:32:07 -0400

    Update get_version.sh

--- travis/get_version.sh
+++ travis/get_version.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-# Output latest version of openvpn
+# Output openvpn version from ubuntu:rolling repository
 echo "$(curl -fsSL https://packages.ubuntu.com/${OS_CODENAME}/openvpn | grep 'Package:' | awk -F '(' {'print $2'} | cut -d \- -f 1)"
