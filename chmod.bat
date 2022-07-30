7c65473210c915393fc18e24193b9e96dbe7084e
stlouisn <stlouisn@hotmail.com>
2022-07-30 10:16:59 -0300

    Create chmod.bat

---
+++ chmod.bat
@@ -0,0 +1,4 @@
+## https://medium.com/@akash1233/change-file-permissions-when-working-with-git-repos-on-windows-ea22e34d5cee
+
+git update-index --chmod=+x rootfs/use/local/bin/docker_entrypoint.sh
+git commit -m "chmod +x"
