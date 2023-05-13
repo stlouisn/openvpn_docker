5e02e4b3dc9a30bba9d74766c23fe6bbe39d88d6
stlouisn <stlouisn@hotmail.com>
2023-05-13 11:40:35 -0300

    Update docker_healthcheck.sh

--- rootfs/usr/local/bin/docker_healthcheck.sh
+++ rootfs/usr/local/bin/docker_healthcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ping -c 1 -W 3 -q google.com > /dev/null && echo OK || (killall -s SIGUSR1 openvpn; exit 1)"]
+ping -c 1 -W 3 -q google.com > /dev/null && echo OK || (killall -s SIGUSR1 openvpn; exit 1)
