d99dcd00576894ee7b8a82968896ba35be5dce83
stlouisn <stlouisn@hotmail.com>
2023-05-13 11:25:35 -0300

    Update chmod.bat

--- scripts/chmod.bat
+++ scripts/chmod.bat
@@ -1,3 +1,4 @@
 git update-index --chmod=+x ..\rootfs\usr\local\bin\docker_entrypoint.sh
+git update-index --chmod=+x ..\rootfs\usr\local\bin\docker_healthcheck.sh
 git ls-files --stage
 git commit -m "chmod +x"
